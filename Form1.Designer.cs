b59d1c03fff8cc4deeb88d92417d5dad8031e822
DaveCS1 <daveshipman@gmail.com>
2024-03-31 16:30:54 -0500

    commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -52,7 +52,7 @@
             this.decryptToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(918, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1071, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -97,20 +97,20 @@
             // 
             this.txtEncryptionKey.Location = new System.Drawing.Point(115, 29);
             this.txtEncryptionKey.Name = "txtEncryptionKey";
-            this.txtEncryptionKey.Size = new System.Drawing.Size(206, 22);
+            this.txtEncryptionKey.Size = new System.Drawing.Size(300, 22);
             this.txtEncryptionKey.TabIndex = 2;
             // 
             // txtDecryptionKey
             // 
-            this.txtDecryptionKey.Location = new System.Drawing.Point(423, 29);
+            this.txtDecryptionKey.Location = new System.Drawing.Point(560, 29);
             this.txtDecryptionKey.Name = "txtDecryptionKey";
-            this.txtDecryptionKey.Size = new System.Drawing.Size(206, 22);
+            this.txtDecryptionKey.Size = new System.Drawing.Size(300, 22);
             this.txtDecryptionKey.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(337, 32);
+            this.label2.Location = new System.Drawing.Point(474, 32);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 16);
             this.label2.TabIndex = 3;
@@ -125,7 +125,7 @@
             this.lstFiles.ItemHeight = 16;
             this.lstFiles.Location = new System.Drawing.Point(16, 74);
             this.lstFiles.Name = "lstFiles";
-            this.lstFiles.Size = new System.Drawing.Size(890, 84);
+            this.lstFiles.Size = new System.Drawing.Size(1043, 84);
             this.lstFiles.TabIndex = 5;
             this.lstFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
             this.lstFiles.DragOver += new System.Windows.Forms.DragEventHandler(this.frmMain_DragOver);
@@ -137,7 +137,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.rtfFileContents.Location = new System.Drawing.Point(16, 164);
             this.rtfFileContents.Name = "rtfFileContents";
-            this.rtfFileContents.Size = new System.Drawing.Size(890, 395);
+            this.rtfFileContents.Size = new System.Drawing.Size(1043, 395);
             this.rtfFileContents.TabIndex = 6;
             this.rtfFileContents.Text = "";
             // 
@@ -145,7 +145,7 @@
             // 
             this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(718, 32);
+            this.checkBox1.Location = new System.Drawing.Point(871, 32);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(175, 20);
             this.checkBox1.TabIndex = 7;
@@ -158,7 +158,7 @@
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 571);
+            this.ClientSize = new System.Drawing.Size(1071, 571);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.rtfFileContents);
             this.Controls.Add(this.lstFiles);
@@ -169,6 +169,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Drag And Drop Files Anywhere";
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
             this.DragOver += new System.Windows.Forms.DragEventHandler(this.frmMain_DragOver);
--- OfuscateAndEncrypt.cs
+++ OfuscateAndEncrypt.cs
@@ -10,41 +10,12 @@ using System.Threading.Tasks;
 namespace EncryptDecryptAndRandomize
 {
     internal class OfuscateAndEncrypt
-    {
-        //        static Dictionary<string, string> nameMapping = new Dictionary<string, string>();
-        //        static Random random = new Random();
-        //        static byte[] key = new byte[32]; // 256 bits
-        //        static byte[] iv = new byte[16]; // 128 bits
-        //       List<string> filesToEncryp = new List<string>();
-        //       public OfuscateAndEncrypt(List<string> filesToEncrypt)
-        //{
-        //    filesToEncryp.Clear();
-        //    foreach (string file in filesToEncrypt)
-        //    {
-        //        random.NextBytes(key);
-        //        random.NextBytes(iv);
-
-        //        string code = File.ReadAllText(file);
-        //        string obfuscatedCode = ObfuscateCode(code);
-        //        string encryptedCode = EncryptString(obfuscatedCode, key, iv);
-        //        File.WriteAllText("EncryptedVBCode.txt", encryptedCode);
-
-        //        // Convert the key to a Base64 string
-        //        string keyString = Convert.ToBase64String(key);
-        //        // Save the key string to a file
-        //        File.WriteAllText("EncryptionKey.txt", keyString);
-        //    }
-        //    SaveMappingToFile("Mapping.txt");
-
-        //  //var t=  DecryptAndDeobfuscate("EncryptedVBCode.txt", "EncryptionKey.txt", "Mapping.txt");
-        ////Console.WriteLine(t);
-        //}
+    {      
         static Dictionary<string, string> nameMapping = new Dictionary<string, string>();
         static Random random = new Random();
         static byte[] key = new byte[32]; // 256 bits
         static byte[] iv = new byte[16]; // 128 bits
         List<string> filesToEncryp = new List<string>();
-
         public string EncryptionKey { get; private set; }
 
         public OfuscateAndEncrypt(List<string> filesToEncrypt)
@@ -54,11 +25,9 @@ namespace EncryptDecryptAndRandomize
             {
                 random.NextBytes(key);
                 random.NextBytes(iv);
-
                 string code = File.ReadAllText(file);
                 string obfuscatedCode = ObfuscateCode(code);
                 string encryptedCode = EncryptString(obfuscatedCode, key, iv);
-
                 // Append the encrypted code to EncryptedVBCode.txt
                 using (StreamWriter writer = File.AppendText("EncryptedVBCode.txt"))
                 {
@@ -67,7 +36,6 @@ namespace EncryptDecryptAndRandomize
                     writer.WriteLine();
                     writer.WriteLine($"***Next File*** {Path.GetFileName(file)}");
                 }
-
                 // Convert the key to a Base64 string and store it in the EncryptionKey property
                 EncryptionKey = Convert.ToBase64String(key);
             }
@@ -77,6 +45,7 @@ namespace EncryptDecryptAndRandomize
             //Console.WriteLine(t);
         }
 
+        #region Encrypt
         static string ObfuscateCode(string code)
         {
             string pattern = @"\b([a-zA-Z_][a-zA-Z0-9_]*)\b";
@@ -131,61 +100,64 @@ namespace EncryptDecryptAndRandomize
                     }
                 }
             }
-        }
-public string DecryptAndDeobfuscate(string encryptedFilePath, string keyFilePath, string mappingFilePath)
-{
-    // Read the encrypted code from the file
-    string encryptedCode = File.ReadAllText(encryptedFilePath);
-
-    // Read the encryption key from the file
-    string keyString = File.ReadAllText(keyFilePath);
-    byte[] key = Convert.FromBase64String(keyString);
-
-    // Decrypt the code
-    string decryptedCode = DecryptString(encryptedCode, key, iv);
-
-    // Load the name mapping from the file
-    LoadMappingFromFile(mappingFilePath);
-
-    // Deobfuscate the code using the name mapping
-    string deobfuscatedCode = DeobfuscateCode(decryptedCode);
-
-    return deobfuscatedCode;
-}
-
-static string DecryptString(string encryptedText, byte[] key, byte[] iv)
-{
-    using (Aes aes = Aes.Create())
-    {
-        aes.Key = key;
-        aes.IV = iv;
-
-        ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-        byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
-
-        using (MemoryStream msDecrypt = new MemoryStream(encryptedBytes))
+        } 
+        #endregion
+        #region Decrypt
+        public string DecryptAndDeobfuscate(string encryptedFilePath, string keyFilePath, string mappingFilePath)
         {
-            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+            // Read the encrypted code from the file
+            string encryptedCode = File.ReadAllText(encryptedFilePath);
+
+            // Read the encryption key from the file
+            string keyString = File.ReadAllText(keyFilePath);
+            byte[] key = Convert.FromBase64String(keyString);
+
+            // Decrypt the code
+            string decryptedCode = DecryptString(encryptedCode, key, iv);
+
+            // Load the name mapping from the file
+            LoadMappingFromFile(mappingFilePath);
+
+            // Deobfuscate the code using the name mapping
+            string deobfuscatedCode = DeobfuscateCode(decryptedCode);
+
+            return deobfuscatedCode;
+        }
+
+        static string DecryptString(string encryptedText, byte[] key, byte[] iv)
+        {
+            using (Aes aes = Aes.Create())
             {
-                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                aes.Key = key;
+                aes.IV = iv;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+
+                using (MemoryStream msDecrypt = new MemoryStream(encryptedBytes))
                 {
-                    return srDecrypt.ReadToEnd();
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
                 }
             }
         }
-    }
-}
 
-static string DeobfuscateCode(string obfuscatedCode)
-{
-    string pattern = @"\b([A-Z0-9]{5})\b";
-    return Regex.Replace(obfuscatedCode, pattern, match =>
-    {
-        string obfuscatedName = match.Value;
-        return nameMapping.FirstOrDefault(x => x.Value == obfuscatedName).Key;
-    });
-}
+        static string DeobfuscateCode(string obfuscatedCode)
+        {
+            string pattern = @"\b([A-Z0-9]{5})\b";
+            return Regex.Replace(obfuscatedCode, pattern, match =>
+            {
+                string obfuscatedName = match.Value;
+                return nameMapping.FirstOrDefault(x => x.Value == obfuscatedName).Key;
+            });
+        } 
+        #endregion
 
 
         //
